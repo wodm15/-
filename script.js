a373c2aa1546dd640eac5780c4e68c04d7f722bf
선재영 <wodud1892@naver.com>
2024-04-25 11:18:18 +0900

    update

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-function fetchTMDBData() {
-    const apiKey = 'f62a4e11263f400c617265700750d002'; // 여기에 본인의 API 키를 넣으세요
+const apiKey = 'f62a4e11263f400c617265700750d002'; 
   
-    const options = {
-      method: 'GET',
-      headers: {
-        accept: 'application/json',
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjJhNGUxMTI2M2Y0MDBjNjE3MjY1NzAwNzUwZDAwMiIsInN1YiI6IjY2Mjc4ZGJhY2I1YzhlMDE4NzQyZmUzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JeQmMX_ZFnsPbVvEUEAGuejOKZV3C3NgOtnsjtcK1qc'
-      }
-    };
-  
-    fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        // 여기에서 데이터를 처리하거나 표시하는 등의 작업을 수행할 수 있습니다.
-      })
-      .catch(error => console.error(error));
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjJhNGUxMTI2M2Y0MDBjNjE3MjY1NzAwNzUwZDAwMiIsInN1YiI6IjY2Mjc4ZGJhY2I1YzhlMDE4NzQyZmUzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JeQmMX_ZFnsPbVvEUEAGuejOKZV3C3NgOtnsjtcK1qc'
   }
-  
-  // HTML 버튼과 JavaScript 함수 연결
-  document.getElementById('fetchButton').addEventListener('click', fetchTMDBData);
+};
+
+
+// fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+// .then(response => response.json())
+// .then(data => {
+// console.log(data);
+// const movie = data.results;
+// console.log(movie);
+// })
+// .catch(error => console.error(error));
+
+function fetchTMDBData() {
+  fetch('https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1', options)
+  .then(response => response.json())
+  .then(response => console.log(response))
+  .catch(err => console.error(err)); 
+}
+
+//DOM 요소 만들기
+const searchInput = document.getElementById('searchInput');
+const serarchButton = document.getElementById('searchButton');
+const movieList = document.getElementsById('movieList');
+
+
+// HTML 버튼과 JavaScript 함수 연결
+document.getElementById('fetchButton').addEventListener('click', fetchTMDBData);
   
